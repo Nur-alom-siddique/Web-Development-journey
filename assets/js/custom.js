8130c97a49141c20d7992af9800ef79f1a91aee7
Nur-alom-siddique <alomofficial707020@gmail.com>
2024-09-15 02:40:00 +0600

    somting edited

---
+++ assets/js/custom.js
@@ -0,0 +1,39 @@
+// *********************Task one**************************
+let t = 1000;
+let a = 700;
+
+console.log(t - a);
+
+
+// **************************Task two*****************
+let  student = {
+  mathematics: 75.25,
+  biology: 65,
+  chemistry: 80,
+  physics: 35.45,
+  bangla: 99.50
+};
+
+ 
+let total = student.bangla + student.biology + student.chemistry + student.mathematics + student.physics;
+
+let result = total / 5;
+console.log(result.toFixed(2));
+
+
+
+// ****************Task three*******************************
+let x = 119;
+let b = 5;
+
+console.log(x % b)
+
+
+
+//  ***********************Task four**********************
+//  it means it's Not a Number , the result of the following codes will be "false" because both are number .
+var p = isNaN('11');
+
+var o = isNaN(2-10);
+
+console.log(o);
