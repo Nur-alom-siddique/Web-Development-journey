7bcc1fc4481ad3461c4b60f62f05e38e7cf7947f
Nur-alom-siddique <alomofficial707020@gmail.com>
2024-09-15 19:06:49 +0600

    task two

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,39 +1,113 @@
-// *********************Task one**************************
-let t = 1000;
-let a = 700;
+// ******task one***********
+let burger = 500;
+let coke = 30;
 
-console.log(t - a);
-
-
-// **************************Task two*****************
-let  student = {
-  mathematics: 75.25,
-  biology: 65,
-  chemistry: 80,
-  physics: 35.45,
-  bangla: 99.50
+if (burger >= 500) {
+  console.log('you got free coke');
+} else {
+  console.log(coke);
 };
 
- 
-let total = student.bangla + student.biology + student.chemistry + student.mathematics + student.physics;
 
-let result = total / 5;
-console.log(result.toFixed(2));
-
-
-
-// ****************Task three*******************************
-let x = 119;
-let b = 5;
-
-console.log(x % b)
+// ******task two***********
+let weight = 65;
+let height = 5.10;
+let bmi = weight / height * 2;
+console.log(bmi);
 
 
+if (bmi < 18.5) {
+  console.log('You are under waight');
+  
+}else if (bmi >= 18.5 && bmi <= 24.9) {
+  console.log(' You are normal');
+  
+}else if (bmi >= 25 && bmi <= 29.9) {
+  console.log('You are overweight');
+  
+} else {
+  console.log('You are obese.');
+  
+}
 
-//  ***********************Task four**********************
-//  it means it's Not a Number , the result of the following codes will be "false" because both are number .
-var p = isNaN('11');
+// ******task three***********
+let finalResult = 99;
 
-var o = isNaN(2-10);
+if (finalResult >= 90) {
+  
+  console.log('A');
+  
+} else if (finalResult >= 80) {
+  console.log('B');
+  
+}else if (finalResult >= 70) {
+  console.log('C');
+  
+}else if (finalResult >= 60) {
+  console.log('D');
+  
+} else {
+  console.log('F');
+  
+}
 
-console.log(o);
+// ******task four***********
+
+let result = 80;
+let fraind = 60;
+
+
+if (result >= 80) {
+  console.log('Enter the House');
+  if (fraind >= 80) {
+    console.log("let's go for a lunch");
+
+  } else {
+    if (fraind >= 60) {
+      console.log('good luck next time, my friend');
+    } else {
+      if (fraind >= 41) {
+        console.log('unseen your massage');
+      } else {
+          console.log("you got 'Blocked'");
+        
+       }
+    }
+  } 
+  
+}else {
+  console.log('Leave the House');
+  
+};
+// ******task five**********
+let num1 = 55;
+let num2 = 50;
+
+let results = num1 > num2 ? num1 * 2 : num1 + num2;
+
+console.log(results);
+
+
+// ******task six**********
+let ticket = 800;
+let age = 18;
+let student = true;
+
+
+if (age < 10) {
+  ticket = 0.00;
+  console.log('Child fare is: ' + ticket.toFixed(2) + "  'free'");
+} else if (age <= 59 && student) {
+  let discount = ticket * 50 / 100;
+  let student = ticket - discount;
+  console.log('Student 50% discount: ' + student);
+
+} else if (age >= 60) { 
+let seniorCitizen = ticket * 15 / 100;
+  let discount = ticket - seniorCitizen;
+console.log('Senior Citizen fare: ' + discount);
+
+}else {
+
+  console.log('Regular fare :' + ticket);
+};
